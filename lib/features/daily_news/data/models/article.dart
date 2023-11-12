22766226932d34ec4c6b47d0fe7a3f639bcb5354
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-11-12 22:49:02 +0330

    FIX data model and entity model

---
+++ lib/features/daily_news/data/models/article.dart
@@ -0,0 +1,34 @@
+import 'package:clean_architecture_practice/features/daily_news/domain/entities/article.dart';
+
+class ArticleModel extends ArticleEntity {
+  const ArticleModel(
+      {required int id,
+      required String author,
+      required String title,
+      required String description,
+      required String url,
+      required String urlToImage,
+      required String publishedAt,
+      required String content})
+      : super(
+            id: id,
+            author: author,
+            title: title,
+            description: description,
+            url: url,
+            urlToImage: urlToImage,
+            publishedAt: publishedAt,
+            content: content);
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      id: json['id'] ?? "",
+      author: json['author'] ?? "",
+      title: json['title'] ?? "",
+      description: json['description'] ?? "",
+      url: json['url'] ?? "",
+      urlToImage: json['urlToImage'] ?? "",
+      publishedAt: json['publishedAt'] ?? "",
+      content: json['content'] ?? "",
+    );
+  }
+}
--- lib/features/daily_news/domain/entities/article.dart
+++ lib/features/daily_news/domain/entities/article.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-
 import 'package:equatable/equatable.dart';
 
 class ArticleEntity extends Equatable {
   final int id;
   final String author;
-  final String tiltle;
+  final String title;
   final String description;
   final String url;
   final String urlToImage;
@@ -15,7 +13,7 @@ class ArticleEntity extends Equatable {
   const ArticleEntity({
     required this.id,
     required this.author,
-    required this.tiltle,
+    required this.title,
     required this.description,
     required this.url,
     required this.urlToImage,
@@ -23,72 +21,12 @@ class ArticleEntity extends Equatable {
     required this.content,
   });
 
-  ArticleEntity copyWith({
-    int? id,
-    String? author,
-    String? tiltle,
-    String? description,
-    String? url,
-    String? urlToImage,
-    String? publishedAt,
-    String? content,
-  }) {
-    return ArticleEntity(
-      id: id ?? this.id,
-      author: author ?? this.author,
-      tiltle: tiltle ?? this.tiltle,
-      description: description ?? this.description,
-      url: url ?? this.url,
-      urlToImage: urlToImage ?? this.urlToImage,
-      publishedAt: publishedAt ?? this.publishedAt,
-      content: content ?? this.content,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    result.addAll({'id': id});
-    result.addAll({'author': author});
-    result.addAll({'tiltle': tiltle});
-    result.addAll({'description': description});
-    result.addAll({'url': url});
-    result.addAll({'urlToImage': urlToImage});
-    result.addAll({'publishedAt': publishedAt});
-    result.addAll({'content': content});
-
-    return result;
-  }
-
-  factory ArticleEntity.fromMap(Map<String, dynamic> map) {
-    return ArticleEntity(
-      id: map['id']?.toInt() ?? 0,
-      author: map['author'] ?? '',
-      tiltle: map['tiltle'] ?? '',
-      description: map['description'] ?? '',
-      url: map['url'] ?? '',
-      urlToImage: map['urlToImage'] ?? '',
-      publishedAt: map['publishedAt'] ?? '',
-      content: map['content'] ?? '',
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory ArticleEntity.fromJson(String source) =>
-      ArticleEntity.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'ArticleEntity(id: $id, author: $author, tiltle: $tiltle, description: $description, url: $url, urlToImage: $urlToImage, publishedAt: $publishedAt, content: $content)';
-  }
-
   @override
   List<Object> get props {
     return [
       id,
       author,
-      tiltle,
+      title,
       description,
       url,
       urlToImage,
