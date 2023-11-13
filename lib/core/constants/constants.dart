1f4236e605414dfa353319cb94b25a8ae2469349
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-11-13 23:58:29 +0330

    add  methods to data repository

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsApiBaseURL = 'https://newsapi.org/v2';
+const String apiKey = '2498fb1dd8d547b09ae0e7854645a1b4';
+const String country = 'us';
+const String category = 'business';
--- lib/features/daily_news/data/repository/article_repository_implementation.dart
+++ lib/features/daily_news/data/repository/article_repository_implementation.dart
@@ -1,11 +1,32 @@
+import 'dart:io';
+
+import 'package:clean_architecture_practice/core/constants/constants.dart';
 import 'package:clean_architecture_practice/core/resources/data_state.dart';
+import 'package:clean_architecture_practice/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:clean_architecture_practice/features/daily_news/data/models/article.dart';
 import 'package:clean_architecture_practice/features/daily_news/domain/repository/article_repository.dart';
+import 'package:dio/dio.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsAiServicec _newsAiServicec;
+
+  ArticleRepositoryImpl(this._newsAiServicec);
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticle() {
-    // TODO: implement getNewsArticle
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticle() async {
+    try {
+      final httpResponse = await _newsAiServicec.getNewsArticles(
+          apiKey: apiKey, category: category, country: country);
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFaild(DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions));
+      }
+    } on DioException catch (e) {
+      return DataFaild(e);
+    }
   }
 }
