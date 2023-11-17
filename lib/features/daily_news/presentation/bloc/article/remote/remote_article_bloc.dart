d619c6b223e84cd4d4731e5cc5bc97d2e05592c0
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-11-17 19:40:06 +0330

    Update remote_article_bloc.dart

--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -18,6 +18,7 @@ class RemoteArticleBloc extends Bloc<RemoteArticleEvent, RemoteArticleState> {
       emit(RemoteArticlesDone(dataState.data!));
     }
     if (dataState is DataFaild) {
+      print(dataState.error!.message);
       emit(RemoteArticlesError(dataState.error!));
     }
   }
