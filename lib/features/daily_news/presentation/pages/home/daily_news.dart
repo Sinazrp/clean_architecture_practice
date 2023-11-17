4e8f200fbbd25f458cfe0929420ac398f8f1f0a9
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-11-17 15:23:39 +0330

    Create daily_news.dart

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily News"),
+      ),
+    );
+  }
+}
