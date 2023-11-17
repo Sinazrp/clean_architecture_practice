881e96c210f1ee174598821d94ee3978291f5358
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-11-17 14:51:40 +0330

    Update remote_article_bloc.dart

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:clean_architecture_practice/core/resources/data_state.dart';
+import 'package:clean_architecture_practice/features/daily_news/domain/usecases/get_article.dart';
+import 'package:clean_architecture_practice/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:clean_architecture_practice/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticleEvent, RemoteArticleState> {
+  final GetArticleUseCase _getArticleUseCase;
+  RemoteArticlesBloc(this._getArticleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticleEvent>(onGetArticleEvent);
+  }
+
+  void onGetArticleEvent(
+      GetArticleEvent event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _getArticleUseCase();
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+    if (dataState is DataFaild) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
